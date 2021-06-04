7ef4614c5ebdc7d2b5437f1054cf8407692649ba
typhonrt <support@typhonrt.org>
2021-06-04 03:42:04 -0700

    update eventbus options

--- src/index.js
+++
@@ -1,2 +0,0 @@
-export *                      from './functions.js';
-export { default as default } from './FileUtilPlugin.js';
---
+++ src/plugin.js
@@ -0,0 +1,35 @@
+import * as FUF from './functions.js';
+
+/**
+ * Wires up FileUtil functions on the plugin eventbus.
+ *
+ * @param {object} ev - PluginInvokeEvent - The plugin event.
+ *
+ * @see https://www.npmjs.com/package/@typhonjs-plugin/manager
+ */
+export function onPluginLoad(ev)
+{
+   const eventbus = ev.eventbus;
+
+   const options = ev.pluginOptions;
+
+   let guard = true;
+
+   // Apply any plugin options.
+   if (typeof options === 'object')
+   {
+      if (typeof options.guard === 'boolean') { guard = options.guard; }
+   }
+
+   eventbus.on(`typhonjs:utils:file:dir:walk`, FUF.walkDir, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:file:has`, FUF.hasFile, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:files:walk`, FUF.walkFiles, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:list:dir:get`, FUF.getDirList, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:list:file:get`, FUF.getFileList, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:path:common`, FUF.commonPath, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:path:common:mapped`, FUF.commonMappedPath, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:path:relative:get`, FUF.getRelativePath, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:sort:path`, FUF.pathSort, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:url:path:dir:get`, FUF.getURLDirpath, void 0, { guard });
+   eventbus.on(`typhonjs:utils:file:url:path:file:get`, FUF.getURLFilepath, void 0, { guard });
+}
